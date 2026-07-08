6826719adf1ec5db7e58afee7919a6e52f300e8a
moein mohammadnia <moeinmohammadnia@yahoo.com>
2026-07-08 17:05:24 +0330

    fix icon bug/shipping-info-comp

--- frontend/src/feature/shipping/components/ShippingInfoItem.tsx
+++ frontend/src/feature/shipping/components/ShippingInfoItem.tsx
@@ -4,7 +4,7 @@ import { type ComponentProps } from "react";
 const ShippingInfoItem = ({ children, ...props }: ComponentProps<"div">) => {
   return (
     <div {...props}>
-      <InfoIcon className="size-4 stroke-t-primary" />
+      <InfoIcon className="size-4 shrink-0 stroke-t-primary" />
       <p className="text-xl">{children}</p>
     </div>
   );
