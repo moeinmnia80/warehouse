c85d50584e026a6c1ac6444e3da5c1a4a0fd95bd
moein mohammadnia <moeinmohammadnia@yahoo.com>
2026-07-31 14:24:47 +0330

    feature: add select to payment and address comp

--- frontend/src/feature/shipping/components/ShippingPaymentMethod.tsx
+++ frontend/src/feature/shipping/components/ShippingPaymentMethod.tsx
@@ -1,3 +1,33 @@
+import { Select, SelectButton, SelectContent, SelectItems } from "@/shared";
+
+const mockData = [
+  { id: "0", value: "1" },
+  { id: "1", value: "2" },
+  { id: "2", value: "3" },
+  { id: "3", value: "4" },
+];
+
 export const ShippingPaymentMethod = () => {
-  return <div>ShippingPaymentMethod</div>;
+  return (
+    <div>
+      <section className="max-w-100 py-6 border-b border-bo-primary">
+        <h3 className="text-sm mb-1.5">Select Payment Method</h3>
+        <Select defaultValue="Select Payment Method">
+          <SelectButton className="btn btn--border justify-between bg-b-primary px-4" />
+          <SelectContent className="w-full bg-b-primary border border-bo-primary rounded-lg mt-1 animate-slide-down">
+            {mockData.map((item) => (
+              <SelectItems
+                key={item.id}
+                className="flex items-center h-12 px-4 cursor-pointer hover:bg-b-secondary"
+                value={item.value}
+                onClick={(selectedValue) => console.log(selectedValue)}
+              >
+                {item.value}
+              </SelectItems>
+            ))}
+          </SelectContent>
+        </Select>
+      </section>
+    </div>
+  );
 };
